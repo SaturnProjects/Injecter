797defe8c258001c28a4f9206cf310ecf56f02df
Saturn <153832419+SaturnProjects@users.noreply.github.com>
2024-04-23 18:26:25 -0500

    Add files via upload

---
+++ SaturnLoader/loader.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SaturnLoader
+{
+    partial class loader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InjectBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.satusText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(209, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SATURN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(202, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "INJECT YOUR DLL";
+            // 
+            // InjectBtn
+            // 
+            this.InjectBtn.Animated = true;
+            this.InjectBtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.InjectBtn.BorderRadius = 5;
+            this.InjectBtn.BorderThickness = 4;
+            this.InjectBtn.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.InjectBtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.InjectBtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.InjectBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.InjectBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.InjectBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.InjectBtn.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InjectBtn.ForeColor = System.Drawing.Color.White;
+            this.InjectBtn.Location = new System.Drawing.Point(176, 237);
+            this.InjectBtn.Name = "InjectBtn";
+            this.InjectBtn.Size = new System.Drawing.Size(197, 45);
+            this.InjectBtn.TabIndex = 2;
+            this.InjectBtn.Text = "INJECT";
+            this.InjectBtn.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.Animated = true;
+            this.guna2Button2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.guna2Button2.BorderRadius = 5;
+            this.guna2Button2.BorderThickness = 4;
+            this.guna2Button2.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.guna2Button2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.Location = new System.Drawing.Point(470, 270);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(84, 30);
+            this.guna2Button2.TabIndex = 3;
+            this.guna2Button2.Text = "INFO";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // satusText
+            // 
+            this.satusText.AutoSize = true;
+            this.satusText.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.satusText.ForeColor = System.Drawing.Color.White;
+            this.satusText.Location = new System.Drawing.Point(242, 253);
+            this.satusText.Name = "satusText";
+            this.satusText.Size = new System.Drawing.Size(32, 17);
+            this.satusText.TabIndex = 4;
+            this.satusText.Text = "???";
+            // 
+            // loader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(566, 312);
+            this.Controls.Add(this.satusText);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.InjectBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "loader";
+            this.ShowIcon = false;
+            this.Text = "Saturn | Loader";
+            this.Load += new System.EventHandler(this.loader_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2Button InjectBtn;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private System.Windows.Forms.Label satusText;
+    }
+}
+
---
+++ SaturnLoader/loader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.CodeDom;
+
+namespace SaturnLoader
+{
+    public partial class loader : Form
+    {
+        public loader()
+        {
+            InitializeComponent();
+        }
+
+        private void loader_Load(object sender, EventArgs e)
+        {
+            satusText.Hide();
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                satusText.Show();
+                satusText.Text = "Injecting...";
+                Process.Start("SaturnInjecter.exe"); // loads the injecter
+                Console.Beep(500, 500);
+                InjectBtn.Hide();
+            } catch 
+            
+            {
+                MessageBox.Show("Could Not Find Injecter File\nMake sure you drag the exe in this directory");
+                Console.Beep(200, 500);
+            }
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Saturn was created by naz! please give credits\n\nhttps://github.com/naziscool/SaturnInjecter.\nSTAR IT OR ELSE YOUR NOT GONNA GET A COOKIE.", "saturn"); ;
+        }
+    }
+}
